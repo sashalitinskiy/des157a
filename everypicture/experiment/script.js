72b4e3c839c6a6a7def5ff141d86e37b9128e858
sashalitinskiy <amlitinskiy@ucdavis.edu>
2025-02-24 13:17:55 -0800

    even more simplification of the overlays in every picture experiment

--- everypicture/experiment/script.js
+++ everypicture/experiment/script.js
@@ -2,68 +2,26 @@
     'use strict';
 
     window.addEventListener('load', function () {
-        const sections = document.querySelectorAll('section');
-        const image = document.querySelector('figure img');
         const overlay = document.getElementById("overlay");
         const overlayText = document.getElementById("overlay-text");
 
-        let sectionTops = [];
-        let pageTop;
-        let counter = 0;
-        let prevCounter = -1;
-        let doneResizing;
-
-        const messages = {
-            0: "Yellow Wax Seal",
-            1: "Orange and Pink Wax Seal"
-        };
-
-        resetPagePosition();
+        const messages = ["Yellow Wax Seal", "Orange and Pink Wax Seal"];
 
         window.addEventListener('scroll', function () {
-            pageTop = window.pageYOffset + window.innerHeight / 3; 
+            let scrollPosition = window.scrollY;
 
-            for (let i = 0; i < sections.length; i++) {
-                if (pageTop >= sectionTops[i]) {
-                    counter = i;
-                }
-            }
-
-            if (counter !== prevCounter) {
-                image.className = 'sect' + (counter + 2); 
-                showOverlay(counter);
-                prevCounter = counter;
-            }
-        });
-
-        window.addEventListener('resize', function () {
-            clearTimeout(doneResizing);
-            doneResizing = setTimeout(resetPagePosition, 500);
-        });
-
-        function resetPagePosition() {
-            sectionTops = [];
-            sections.forEach(section => {
-                sectionTops.push(section.getBoundingClientRect().top + window.pageYOffset);
-            });
-
-            pageTop = window.pageYOffset + window.innerHeight / 3;
-            counter = 0;
-
-            for (let i = 0; i < sectionTops.length; i++) {
-                if (pageTop >= sectionTops[i]) {
-                    counter = i;
-                }
-            }
-        }
-
-        function showOverlay(index) {
-            if (messages[index]) {
-                overlayText.innerText = messages[index];
-                overlay.style.display = "block";
+            if (scrollPosition > 200 && scrollPosition < 600) {
+                showOverlay(0); 
+            } else if (scrollPosition >= 600) {
+                showOverlay(1); 
             } else {
                 overlay.style.display = "none";
             }
+        });
+
+        function showOverlay(index) {
+            overlayText.innerText = messages[index];
+            overlay.style.display = "block";
         }
 
         window.closeOverlay = function () {
